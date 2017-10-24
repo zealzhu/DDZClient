ddd1c7fdb6e59a6d14ba4c1ace6a1a51142c8c35
zhu <z5130761@gmail.com>
2017-10-24 18:11:35 +0800

    添加线程以及网络模块

--- Classes/FrameWork/Thread/ThreadException.h
+++ Classes/FrameWork/Thread/ThreadException.h
@@ -1,11 +1,12 @@
-//////////////////////////////////////////////////////////
-//// Author:	Zhu 
-//// Date:		2017/10/23
-//// Describe:	线程异常类
-/////////////////////////////////////////////////////////
-#pragma once
-#ifndef _THREAD_EXCEPTION_H
-#define _THREAD_EXCEPTION_H
+// MUD Programming
+// Ron Penton
+// (C)2003
+// ThreadException.h - Main header for the thread exceptions.
+// This holds the ThreadException class, error codes, and functions
+// to print the error codes.
+
+#ifndef THREADEXCEPTION_H
+#define THREADEXCEPTION_H
 
 #include <exception>
 
@@ -13,42 +14,46 @@
 //  Include Files for the threading libraries
 // ============================================================================
 #ifdef WIN32                // windows95 and above
-#include <windows.h>
+    #include <windows.h>
 #else                       // linux
-#include <pthread.h>
+    #include <pthread.h>
 #endif
 
-namespace ThreadLib {
-	enum Error
-	{
-		Unspecified,                // 未说明的错误
-		InitFailure,                // 未初始化
-		CreationFailure             // 创建错误
-	};
 
-	class Exception
-		: public std::exception
-	{
-	public:
-		// ====================================================================
-		// Description: 创建异常
-		// ====================================================================
-		Exception(Error p_error = Unspecified)
-		{
-			m_error = p_error;
-		}
+namespace ThreadLib
+{
 
-		// ====================================================================
-		// Description: 获取错误类型
-		// ====================================================================
-		Error GetError() const
-		{
-			return m_error;
-		}
+    enum Error
+    {
+        Unspecified,                // unspecified error
+        InitFailure,                // thread library not initialized
+        CreationFailure             // thread cannot be created
+    };
 
-	protected:
-		Error m_error;
-	};
+
+    class Exception : public std::exception
+    {
+    public:
+        // ====================================================================
+        // Description: set the error code of the exception, with a default of 
+        //              Unspecified.
+        // ====================================================================
+        Exception( Error p_error = Unspecified )
+        {
+            m_error = p_error;
+        }
+
+        // ====================================================================
+        // Description: Gets the error code of the exception
+        // ====================================================================
+        Error GetError() const
+        {
+            return m_error;
+        }
+
+    protected:
+        Error m_error;
+    };
 }
 
-#endif // !_THREAD_EXCEPTION_H
+#endif
--- Classes/FrameWork/Thread/ThreadLib.h
+++ Classes/FrameWork/Thread/ThreadLib.h
@@ -1,6 +1,10 @@
-#pragma once
-#ifndef _THREAD_LIB_H
-#define _THREAD_LIB_H
+// MUD Programming
+// Ron Penton
+// (C)2003
+// ThreadLib.h - Main header for the thread library.
+
+#ifndef THREADLIB_H
+#define THREADLIB_H
 
 // ============================================================================
 //  Include Files for the threading libraries
